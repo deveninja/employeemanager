0e7395e3d8fb62f3c558d434316c2ac47180edf7
deveninja <abbays10@gmail.com>
2018-01-17 21:32:12 +0800

    Added bob in navBar

--- src/components/navBar.vue
+++ src/components/navBar.vue
@@ -4,6 +4,9 @@
           <div class="container">
               <router-link to='/' class="brand-logo">Employee Manager</router-link>
           </div>
+          <div>
+              Bob
+          </div>
       </div>
       
   </nav>
