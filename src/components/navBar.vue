6262d174edc71c309a3ca3a5b80a8b373c336b7b
deveninja <abbays10@gmail.com>
2018-01-18 00:14:50 +0800

    5th Commit

--- src/components/navBar.vue
+++ src/components/navBar.vue
@@ -1,13 +1,9 @@
 <template>
-  <nav>
-      <div class="nav-wrapper green">
+    <nav>
+        <div class="nav-wrapper green">
           <div class="container">
               <router-link to='/' class="brand-logo">Employee Manager</router-link>
           </div>
-          <div>
-              Bob
-          </div>
-      </div>
-      
-  </nav>
+        </div>
+    </nav>
 </template>
--- src/components/newEmployee.vue
+++ src/components/newEmployee.vue
@@ -1,6 +1,16 @@
 <template>
   <div id="new-employee">
       <h3>New Employee</h3>
+      <div class="row">
+        <form @submit.prevent="saveEmployee" class="col s12">
+          <div class="row">
+            <div class="input-field col s12">
+              <input type="text" v-model="employee_id" required>
+              <label for="">Employee ID#</label>
+            </div>
+          </div>
+        </form>
+      </div>
   </div>
 </template>
 
