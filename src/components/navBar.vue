28447601d5149f6120c5f7ddd23bad402f964671
deveninja <abbays10@gmail.com>
2018-01-17 21:09:55 +0800

    Added another div in navBar

--- src/components/navBar.vue
+++ src/components/navBar.vue
@@ -5,5 +5,8 @@
               <router-link to='/' class="brand-logo">Employee Manager</router-link>
           </div>
       </div>
+      <div class="another-div">
+          
+      </div>
   </nav>
 </template>
