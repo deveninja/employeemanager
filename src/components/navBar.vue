cb74a1f6e58b98c16c4c713b3eeb92c35f4bdffb
deveninja <abbays10@gmail.com>
2018-01-17 21:27:18 +0800

    Another commit

--- src/components/navBar.vue
+++ src/components/navBar.vue
@@ -5,8 +5,6 @@
               <router-link to='/' class="brand-logo">Employee Manager</router-link>
           </div>
       </div>
-      <div class="another-div">
-          <h1>Push to Github</h1>
-      </div>
+      
   </nav>
 </template>
